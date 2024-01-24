60afa5aa5bdc712e4342aa435783e8163092e505
Rami KHADDOUR <ramikhaddour@gmail.com>
2024-01-24 12:57:58 +0100

    importer bootstrap

---
+++ src/Entity/Categorys.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorysRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CategorysRepository::class)]
+class Categorys
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:2,max:50)]
+    private ?string $cat_name = null;
+
+    #[ORM\OneToMany(mappedBy: 'categoryid', targetEntity: Property::class)]
+    private Collection $properties;
+
+    public function __construct()
+    {
+        $this->properties = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCatName(): ?string
+    {
+        return $this->cat_name;
+    }
+
+    public function setCatName(string $cat_name): static
+    {
+        $this->cat_name = $cat_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Property>
+     */
+    public function getProperties(): Collection
+    {
+        return $this->properties;
+    }
+
+    public function addProperty(Property $property): static
+    {
+        if (!$this->properties->contains($property)) {
+            $this->properties->add($property);
+            $property->setCategoryid($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProperty(Property $property): static
+    {
+        if ($this->properties->removeElement($property)) {
+            // set the owning side to null (unless already changed)
+            if ($property->getCategoryid() === $this) {
+                $property->setCategoryid(null);
+            }
+        }
+
+        return $this;
+    }
+}
